261c805053d2390c8f707f0e230598740749b05c
Soumyak Ranjan Sahoo <136055717+SoumyakRanjanSahoo@users.noreply.github.com>
2025-02-02 06:28:59 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const userid=1212
+let UserEmail="Soumyak@gmail.com"
+var password="@Soumyak27"//prefer not to use var because of issue in block scope and functional scope
+usercity="Bhubaneswar"
+let userState="Odisha"
+
+// userid=121 we ca not change const value
+
+UserEmail="SahooSoumya.google.com"
+password="2929892"
+userState="Karantaka"
+usercity="bangalore"
+
+console.log(userid);
+console.table([UserEmail,password,usercity,userState])
+
