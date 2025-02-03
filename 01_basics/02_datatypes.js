b084009089c94c593adec2356493834bcd2a265c
Soumyak Ranjan Sahoo <136055717+SoumyakRanjanSahoo@users.noreply.github.com>
2025-02-03 06:39:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//  alert(3+3);we are using NodeJS not browser
+ console.log(3+4) 
+
+console.log("Soumyak")
+
+
+// number==> 2 topower 53
+// BigInt
+// String==>""
+// Boolean==>true/false
+// null==> standalone value
+// undefined==>not define
+// Symbol==>unique components
+
+
+// object==>
+
+
+console.log(typeof "Soumyak")//string
+console.log(typeof 12)//integer
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+console.log(typeof true)//boolean
+
