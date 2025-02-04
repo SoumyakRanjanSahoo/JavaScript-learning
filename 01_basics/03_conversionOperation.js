814f458b6544871bd893a1511c67ab70c874fdc2
Soumyak Ranjan Sahoo <136055717+SoumyakRanjanSahoo@users.noreply.github.com>
2025-02-04 14:23:24 +0000

    null,undefined conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score="Soumyak"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33"==>33
 //"33abc"==> NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let isLogggedIn ="Soumyak"
 let booleanIsLoggedIn=Boolean(isLogggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 ==>true; 0==>false
 //""==>false
@@ -21,9 +21,38 @@ console.log(booleanIsLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
+// console.log(1+2);
+// console.log("1"+2);
 
+// console.log(true);
+// console.log(false);
+
+// console.log(+true);
+// console.log(+false);
+
+
+// let a=null
+// console.log (+null)
+
+// console.log(+undefined);
+
+// let game=100;
+// ++game;
+// console.log(game);
+
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null>0)
+
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined>0);
+
+// console.log("2"===2);
+
+//=== strictly checked both data type and value
